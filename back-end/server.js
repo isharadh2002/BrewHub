d9ca3219265008e0b0da599fe80e04841acd4260
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2025-06-10 20:51:15 +0530

    Minor changes

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express();
+const router = express.Router();
+
 require('dotenv').config();
-
-
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, ()=>{console.log(`Server is running. Listening on http://localhost:${PORT}`)});
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello there.....")
+});
